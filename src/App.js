52c7a3334b0decc47e8528daa497ede366cd2564
Ryan Hunter <rshunter1982@gmail.com>
2024-08-09 00:12:48 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import logo from './NoiseSmith_Logo.svg';
 import './App.css';
+import ReactPlayer from 'reactplayer';
 
 function App() {
   return (
     <div className="App">
           <header className="App-header">
-
+              <video autoplay muted loop className="backgroundVideo">
+                  <source
+                      src="https://ugc.production.linktr.ee/54327e82-0d80-45a5-94db-20fa65e1566e_production-id-4990319--1080p-.mp4"
+                      type="video/mp4" />
+              </video>
         <img src={logo} className="App-logo" alt="logo"/>
               <h1>Noisesmith</h1>
       </header>
           <body className="App-body">
-              <video autoplay muted loop className="backgroundVideo">
-                  <source
-                      src="https://ugc.production.linktr.ee/54327e82-0d80-45a5-94db-20fa65e1566e_production-id-4990319--1080p-.mp4"
-                      type="video/mp4"/>
-              </video>
         <h1>Custom Section</h1>
       </body>
       <footer className="App-footer">
