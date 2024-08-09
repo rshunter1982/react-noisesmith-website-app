1b250945c3f34416d8ee7cc7bb32fb3783bad5b9
Ryan Hunter <rshunter1982@gmail.com>
2024-08-08 23:55:27 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+          <header className="App-header">
+
         <img src={logo} className="App-logo" alt="logo"/>
               <h1>Noisesmith</h1>
       </header>
-      <body className="App-body">
+          <body className="App-body">
+              <video autoplay muted loop id="backgroundVideo">
+                  <source
+                      src="https://ugc.production.linktr.ee/54327e82-0d80-45a5-94db-20fa65e1566e_production-id-4990319--1080p-.mp4"
+                      type="video/mp4"/>
+              </video>
         <h1>Custom Section</h1>
       </body>
       <footer className="App-footer">
