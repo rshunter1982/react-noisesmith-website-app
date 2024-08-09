114804d4fe3fc00e75ee089132ee0944a95a8ec1
Ryan Hunter <rshunter1982@gmail.com>
2024-08-09 00:18:18 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -3,25 +3,20 @@ import './App.css';
 import ReactPlayer from 'reactplayer';
 
 function App() {
-  return (
-    <div className="App">
-          <header className="App-header">
-              <video autoplay muted loop className="backgroundVideo">
-                  <source
-                      src="https://ugc.production.linktr.ee/54327e82-0d80-45a5-94db-20fa65e1566e_production-id-4990319--1080p-.mp4"
-                      type="video/mp4" />
-              </video>
-        <img src={logo} className="App-logo" alt="logo"/>
-              <h1>Noisesmith</h1>
-      </header>
-          <body className="App-body">
-        <h1>Custom Section</h1>
-      </body>
-      <footer className="App-footer">
-        <h1>footer</h1>
-      </footer>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1>Noisesmith</h1>
+            </header>
+            <body className="App-body">
+                <h1>Under Construction will be up soon</h1>
+            </body>
+            <footer className="App-footer">
+                <h1>Noisesmith Music</h1>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
