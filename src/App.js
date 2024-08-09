d753ff19e09966bcad956ee9432f00b65a00a758
Ryan Hunter <rshunter1982@gmail.com>
2024-08-08 22:28:14 -0400

    Setup

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1>Noisesmith</h1>
+      </header>
+      <body className="App-body">
+        <h1>body</h1>
+      </body>
+      <footer className="App-footer">
+        <h1>footer</h1>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
