7bf81f046838954a3ab33b521de4c7526c1b075f
Ryan Hunter <rshunter1982@gmail.com>
2024-08-14 23:24:28 -0400

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import musicLink from "../assets/e79f0662-67b1-4d77-b9e1-cc88f867e702_70871fda-e69a-4743-83c1-177f6dbbc7bf.webp"
+import blowYourMindRemix from "../assets/BlowYourMindRemix.jpeg"
 
 const Home = () => {
     return (
         <div>
             <h1>Welcome to Noisesmith</h1>
             <Link to="https://www.beatport.com/track/blow-your-mind-feat-quannum-logic-burnboy-nikal-fieldz-q-the-music-remix/19275925">
-                <img src={musicLink} alt="musicLink" type="image/webp" />
+                <img src={blowYourMindRemix} alt="BlowYourMindRemix" type="image/jpeg" />
             </Link>
         </div>
     );
